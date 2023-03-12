b5b5515c7d33c88fd35ec83dd04a3f77c95e411d
Muhammad Umar Aziz Khan <105535313+MuhammadUmar969@users.noreply.github.com>
2023-03-12 21:20:28 +0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ClickMe from './ClickMe'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ClickMe />
+  </React.StrictMode>,
+)
